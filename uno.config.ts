ac36b04d6243a3f2bf3cf3fc918a3fc23cc57d57
陈治磊 <image72@msn.com>
2023-06-06 09:14:59 +0800

    feat: forks init

---
+++ uno.config.ts
@@ -0,0 +1,46 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTagify,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  presetWind,
+  transformerCompileClass,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss';
+
+export default defineConfig({
+  shortcuts: [
+    ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+  ],
+  presets: [
+    presetUno(),
+    presetWind(),
+    presetAttributify({
+      prefix: 'un-',
+      prefixedOnly: true,
+    }),
+    presetIcons({
+      scale: 1.2,
+      warn: true,
+    }),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        sans: 'DM Sans',
+        serif: 'DM Serif Display',
+        mono: 'DM Mono',
+      },
+    }),
+    presetTagify({ prefix: 'un-' }),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+    transformerCompileClass(),
+  ],
+});
